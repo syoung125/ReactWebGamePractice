6bdf4db7c45da65346e756ddcfcfb82e78dffb6e
syoung125 <laura120555@gmail.com>
2020-08-27 10:12:23 +0900

    First Commit

---
+++ src/components/Lec1GuGuDan.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Lec1GuGuDan extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>GuGuDan</h1>
+      </>
+    );
+  }
+}
+
+export default Lec1GuGuDan;
