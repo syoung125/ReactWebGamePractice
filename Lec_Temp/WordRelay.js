558a0cfc47d15eee4fe55d013fde9b41c0e89444
syoung125 <laura120555@gmail.com>
2020-08-29 16:03:27 +0900

    Complete WordRelay Game logic

--- Lec_Temp/WordRelay.js
+++ Lec_Temp/WordRelay.js
@@ -3,11 +3,56 @@ const { Component } = React;
 
 class WordRelay extends Component {
   state = {
-    text: "Hello, webpack",
+    word: "고서영", // 첫단어
+    value: "",
+    result: "",
+  };
+
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      this.setState({
+        result: "딩동댕",
+        word: this.state.value,
+        value: "",
+      });
+      this.input.focus();
+    } else {
+      this.setState({
+        result: "땡",
+        value: "",
+      });
+      this.input.focus();
+    }
+  };
+
+  onChangeInput = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  input;
+
+  onRefInput = (c) => {
+    this.input = c;
   };
 
   render() {
-    return <h1>{this.state.text}</h1>;
+    return (
+      <>
+        <div>{this.state.word}</div>
+        {/* <form onSubmit={this.onSubmitForm}> */}
+        <input
+          ref={this.onRefInput}
+          value={this.state.value}
+          onChange={this.onChangeInput}
+        />
+        <button onClick={this.onSubmitForm}>입력!</button>
+        {/* </form> */}
+        <div>{this.state.result}</div>
+      </>
+    );
   }
 }
 
--- Lec_Temp/webpack.config.js
+++ Lec_Temp/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
             ],
             "@babel/preset-react",
           ],
-          plugins: [],
+          plugins: ["@babel/plugin-proposal-class-properties"],
         },
       },
     ],
