fe84f615b418442e41bf389741e28fdde5165a8c
syoung125 <laura120555@gmail.com>
2020-08-29 16:49:27 +0900

    Add WordRelayH.js - WordRelay React Hook version

--- Lec_Temp/client.jsx
+++ Lec_Temp/client.jsx
@@ -2,8 +2,10 @@ const React = require("react");
 const ReactDom = require("react-dom");
 const { hot } = require("react-hot-loader/root");
 
-const WordRelay = require("./WordRelay");
+// const WordRelay = require("./WordRelay");
+const WordRelayH = require("./WordRelayH");
 
-const Hot = hot(WordRelay);
+// const Hot = hot(WordRelay);
+const HotH = hot(WordRelayH);
 
-ReactDom.render(<Hot />, document.querySelector("#root"));
+ReactDom.render(<HotH />, document.querySelector("#root"));
