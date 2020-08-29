02fd327f36627e0afda4400893012875eed11fa0
syoung125 <laura120555@gmail.com>
2020-08-29 15:24:17 +0900

    2-7 Exploring webpack:

--- Lec1_GuGuDan/webpack.config.js
+++ Lec1_GuGuDan/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "development", // 개발자: development, 실서비스: production
@@ -6,7 +7,7 @@ module.exports = {
   resolve: {
     extensions: [".jsx", ".js"],
   },
-
+  // webpack 메인: entry, moduel, output
   entry: {
     app: "./client",
   },
@@ -14,16 +15,38 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        loader: "babel-loader",
+        loader: "babel-loader", // entry에 babel-loader를 적용하는거
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
+          // babel-loader에 대한 option들
+          // persets: plugins의 모음
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                // preset-env에 대한 option설정
+                // ex) targets.browsers -> 원하는 browser에만 맞춰서 바꿀수 있음
+                // 참고) 브라우저 리스트 볼수있음: https://github.com/browserslist/browserslist
+                targets: {
+                  browsers: ["> 1% in KR"],
+                },
+                debug: true,
+              },
+            ],
+            "@babel/preset-react",
+          ],
           plugins: [],
         },
       },
     ],
   },
+  plugins: [new webpack.LoaderOptionsPlugin({ debug: true })], // plugin: 확장프로그램
   output: {
     path: path.join(__dirname, "dist"),
     filename: "app.js",
   },
 };
+
+/*
+Webpack 공식문서 확인하기 
+https://webpack.js.org/concepts/
+*/
