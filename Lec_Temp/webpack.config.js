07cbfad00519744c444e0f68db5c7d09d54594b3
syoung125 <laura120555@gmail.com>
2020-08-29 16:14:43 +0900

    Apply webpack-dev-server and hot-loader

--- Lec_Temp/webpack.config.js
+++ Lec_Temp/webpack.config.js
@@ -30,7 +30,10 @@ module.exports = {
             ],
             "@babel/preset-react",
           ],
-          plugins: ["@babel/plugin-proposal-class-properties"],
+          plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "react-hot-loader/babel",
+          ],
         },
       },
     ],
