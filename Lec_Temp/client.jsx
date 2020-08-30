d1271eecda0338a212923441a27cd9ecc9a45b42
syoung125 <laura120555@gmail.com>
2020-08-30 18:07:51 +0900

    Lec3 Add NumberBaseBall Hook version

--- Lec_Temp/client.jsx
+++ Lec_Temp/client.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDom from "react-dom";
 import { hot } from "react-hot-loader/root";
 
-import NumberBaseBall from "./NumberBaseBall.js";
+// import NumberBaseBall from "./NumberBaseBall.js";
+import NumberBaseBallH from "./NumberBaseBallH";
 
-const Hot = hot(NumberBaseBall);
+// const Hot = hot(NumberBaseBall);
+const Hot = hot(NumberBaseBallH);
 
 ReactDom.render(<Hot />, document.querySelector("#root"));
