1bb171d324b2198f645fe8ad4cda44922983095b
syoung125 <laura120555@gmail.com>
2020-09-07 21:48:33 +0900

    Complete Lec4

--- Lec4_ResponseCheck/client.jsx
+++ Lec4_ResponseCheck/client.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDom from "react-dom";
 import { hot } from "react-hot-loader/root";
 
-// import ResponseCheckH from "./ResponseCheckH";
-// const Hot = hot(ResponseCheckH);
+// import ResponseCheck from "./ResponseCheck";
+// const Hot = hot(ResponseCheck);
+
+import ResponseCheckH from "./ResponseCheckH";
+const Hot = hot(ResponseCheckH);
 
 ReactDom.render(<Hot />, document.querySelector("#root"));
