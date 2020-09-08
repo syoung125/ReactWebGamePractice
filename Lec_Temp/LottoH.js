930f9020861123eb9264e0c26df4d7c38c35e6f5
syoung125 <laura120555@gmail.com>
2020-09-08 13:16:44 +0900

    Lec6. useMemo, useCallback

--- Lec_Temp/LottoH.js
+++ Lec_Temp/LottoH.js
@@ -1,4 +1,10 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useMemo,
+  useCallback,
+} from "react";
 import Ball from "./Ball";
 
 // state 안쓰면 분리하기!
@@ -20,7 +26,8 @@ function getWinNumbers() {
 }
 
 const LottoH = () => {
-  const [winNumbers, setWinNumbers] = useState(getWinNumbers());
+  const lottoNumbers = useMemo(() => getWinNumbers(), []); // Hooks가 이 값을 기억하고있음
+  const [winNumbers, setWinNumbers] = useState(lottoNumbers);
   const [winBalls, setWinBalls] = useState([]);
   const [bonus, setBonus] = useState(null);
   const [redo, setRedo] = useState(false);
@@ -50,14 +57,16 @@ const LottoH = () => {
     }, 7000);
   };
 
-  const onClickRedo = () => {
+  const onClickRedo = useCallback(() => {
+    console.log("onClickRedo");
+    console.log(winNumbers); // 너무 기억을 잘 하고 있어서 문제(처음 당첨숫자를 계~속 기어가고있음)
     setWinNumbers(getWinNumbers());
     setWinBalls([]);
     setBonus(null);
     setRedo(false);
 
     timeouts.current = [];
-  };
+  }, [winNumbers]); // input에 바뀔 값을 넣어줌(까먹어야하는 값)
 
   return (
     <>
@@ -77,3 +86,10 @@ const LottoH = () => {
 export default LottoH;
 
 // Hooks 동작 완료, But getWinNumbers가 계속 실행되는 문제!!
+// 왜? 전체가 계속 다시 실행되기 때문에 getNubmers가 계속 다시 실행
+// 로또 숫자 -> 다시 실행하지 않고 기억(캐싱) 하기 위해 useMemo 사용
+
+// useMemo: 복잡한 함수의 결과값을 기억 (리턴값)
+// useRef: 일반 값을 기억
+// useCallback: 함수 자체를 기억
+// ** 자식 컴포넌트에 함수를 넘길 때에는 useCallback을 꼭 해줘야 함
