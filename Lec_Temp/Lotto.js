fbd96be8b7f4aae99c312963baccb6a57c84ca35
syoung125 <laura120555@gmail.com>
2020-09-08 12:46:36 +0900

    Lec6. Complete Lotto class version

---
+++ Lec_Temp/Lotto.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import Ball from "./Ball";
+
+function getWinNumbers() {
+  console.log("getWinNumbers");
+  const candidate = Array(45)
+    .fill()
+    .map((v, i) => i + 1);
+  const shuffle = [];
+  // 1-45 무작위 순서로 정렬
+  while (candidate.length > 0) {
+    shuffle.push(
+      candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
+    );
+  }
+  const bonusNumber = shuffle[shuffle.length - 1]; // 그중 맨마지막 숫자
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c); // 그중 앞에서 6개 -> 오름차순으로 정렬
+  return [...winNumbers, bonusNumber];
+}
+
+class Lotto extends Component {
+  state = {
+    winNumbers: getWinNumbers(),
+    winBalls: [],
+    bonus: null,
+    redo: false,
+  };
+
+  timeouts = [];
+
+  runTimeouts = () => {
+    const { winNumbers } = this.state;
+    // let을 사용하면 클로저 문제가 안남
+    for (let i = 0; i < winNumbers.length - 1; i++) {
+      this.timeouts[i] = setTimeout(() => {
+        this.setState((prevState) => {
+          return {
+            winBalls: [...prevState.winBalls, winNumbers[i]],
+          };
+        });
+      }, (i + 1) * 1000);
+    }
+    this.timeouts[6] = setTimeout(() => {
+      this.setState({
+        bonus: winNumbers[6],
+        redo: true,
+      });
+    }, 7000);
+  };
+
+  componentDidMount() {
+    this.runTimeouts();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // console.log("componentDidUpdate");
+    if (this.state.winBalls.length === 0) {
+      this.runTimeouts();
+    }
+  }
+
+  componentWillUnmount() {
+    this.timeouts.forEach((v) => {
+      clearTimeout(v);
+    });
+  }
+
+  onClickRedo = () => {
+    this.setState({
+      winNumbers: getWinNumbers(),
+      winBalls: [],
+      bonus: null,
+      redo: false,
+    });
+
+    this.timeouts = [];
+  };
+
+  render() {
+    const { winBalls, bonus, redo } = this.state;
+    return (
+      <>
+        <div>당첨숫자</div>
+        <div id="결과창">
+          {winBalls.map((v) => (
+            <Ball key={v} number={v} />
+          ))}
+        </div>
+        <div>보너스</div>
+        <div>{bonus && <Ball number={bonus} />}</div>
+        {redo && <button onClick={this.onClickRedo}>한번 더!</button>}
+      </>
+    );
+  }
+}
+
+export default Lotto;
--- Lec_Temp/client.jsx
+++ Lec_Temp/client.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import ReactDom from "react-dom";
 import { hot } from "react-hot-loader/root";
 
-// import RSP from "./RSP";
-// const Hot = hot(RSP);
-
-import RSPH from "./RSPH";
-const Hot = hot(RSPH);
+import Lotto from "./Lotto";
+const Hot = hot(Lotto);
 
 ReactDom.render(<Hot />, document.querySelector("#root"));
